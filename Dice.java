1f89d7f388b76e264bb4e879ac8f96ac0f0c9cfb
Jeremy Trane <jeremytrane@dhcp-892b0554.ucd.ie>
2022-10-18 15:45:31 +0100

    updated dice

--- Dice.java
+++ Dice.java
@@ -1,7 +1,22 @@
+import java.util.Random;
 
 public class Dice {
 
-    public Dice() {
+    private int diceOne;
+	private int diceTwo;
+
+    public Dice(Dice d) {
+        diceOne = d.diceOne;
+		diceTwo = d.diceTwo;
+        int[] possibleDiceRolls = {1, 2, 3, 4, 5, 6};
+	}
+	
+	public int getDiceOne(){
+		return diceOne;
+	}
+	
+	public int getDiceTwo(){
+		return diceTwo;
 	}
 
 }
