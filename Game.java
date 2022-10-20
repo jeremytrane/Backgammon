6746002117793446f92cdbb3198ba25d4438ed59
jeremytrane <jeremy.trane@ucdconnect.ie>
2022-10-20 17:44:55 +0100

    up board.java and game.java

--- Game.java
+++ Game.java
@@ -4,7 +4,7 @@ public class Game {
     public static void main(String[] args) { 
 
         Scanner in = new Scanner(System.in);
-        String name1, name2;
+        String name1, name2, move;
         final int INITIAL_CHECKERS=15;
 
         System.out.println("Please enter name of player 1:");
@@ -14,6 +14,17 @@ public class Game {
 
         Player player_1 = new Player(name1,INITIAL_CHECKERS);
         Player player_2 = new Player(name2,INITIAL_CHECKERS);
+        Board board = new Board();
+
+        while (player_1.getCheckerCount()!=0 || player_2.getCheckerCount()!=0) {
+            board.Draw();
+            System.out.println("Please enter your move");
+            move = in.nextLine();
+            if (move.equalsIgnoreCase("Q")) {
+                break;
+            }
+
+        }
 
 
         in.close();
