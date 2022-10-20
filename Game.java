a21edf3817d94f18ee4b4edb83e1b80cdce0e5c0
JamieMadden38 <jamiemadden38@gmail.com>
2022-10-20 16:44:20 +0100

    Up get player names

--- Game.java
+++ Game.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
 
 public class Game {
-
     public static void main(String[] args) { 
+        Scanner in = new Scanner(System.in);
+        String player1;
+        String player2;
+
+        System.out.println("Please enter name of player 1:");
+        player1 = in.nextLine();
+        System.out.println("Please enter name of player 2");
+        player2 = in.nextLine();
+
+        Player players = new Player(player1,player2);
+
         Dice firstDice = new Dice();
         firstDice.rollDice(firstDice);
         System.out.println(firstDice.getDots());
+
+        in.close();
     }
 
 }
--- Player.java
+++ Player.java
@@ -1,8 +1,18 @@
 
 public class Player {
+    private String name1;
+    private String name2;
 
-    public Player(){ 
-        
+    public Player(String n1, String n2){ 
+        name1 = n1;
+        name2 = n2;
+    }
+
+    public String getPlayer1(){
+        return name1;
+    } 
+    public String getPlayer2(){
+        return name2;
     }
 
 }
