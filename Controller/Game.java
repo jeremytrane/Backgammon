473fd90603eeda343fa7de4bbffb0dfdb794f46e
jeremytrane <jeremy.trane@ucdconnect.ie>
2022-10-27 12:36:44 +0100

    up

--- Controller/Game.java
+++ Controller/Game.java
@@ -1,18 +1,20 @@
 package Controller;
 
 import java.util.Scanner;
-
-import Model.Player;
-import Model.Dice;
-import Setup.Setup;
+import Model.*;
+import Setup.*;
+import View.Board;
 
 public class Game {
 
     public static void PlayGame() {
 
+        Setup.setupMessage();
+
         Scanner in = new Scanner(System.in);
 
-        Dice[] bothDie = Setup.setupAndRun();
+        Dice[] bothDie = Setup.setupDie();
+        Points[] allPoints = Setup.defaultBoard();
         
         System.out.println("Please enter name of player 1:");
         Player player_1 = Setup.setupPlayer(in);
@@ -20,6 +22,9 @@ public class Game {
         Player player_2 = Setup.setupPlayer(in);
 
         while (player_1.getCheckerCount()!=0 || player_2.getCheckerCount()!=0) {
+
+            Board.Draw(allPoints, bothDie);
+
             System.out.println("Please enter your move");
             String move = in.nextLine();
             if (move.equalsIgnoreCase("Q")) {
@@ -27,7 +32,6 @@ public class Game {
             } else if (move.equalsIgnoreCase("R")) {
                 Dice.rollDie(bothDie);
             }
-            
         }
 
         in.close();
--- Setup/Setup.java
+++ Setup/Setup.java
@@ -10,13 +10,7 @@ import View.Board;
 
 public class Setup {
 
-    public static Dice[] setupAndRun() {
-        setupMessage();
-        Dice[] bothDie = setupPieces();
-        return bothDie;
-    }
-
-private static void setupMessage() {
+public static void setupMessage() {
     System.out.println(" _                _                                                   ");
     System.out.println("| |              | |                                                  ");
     System.out.println("| |__   __ _  ___| | ____ _  __ _ _ __ ___  _ __ ___   ___  _ __      ");
@@ -28,46 +22,14 @@ private static void setupMessage() {
 
 }
 
-private static Dice[] setupPieces() {
-
-        Points[] allPoints = new Points[24];
-        allPoints[0] = new Points(Colour.W, 5);
-        allPoints[1] = new Points(null, 0);
-        allPoints[2] = new Points(null, 0);
-        allPoints[3] = new Points(null, 0);
-        allPoints[4] = new Points(Colour.B, 3);
-        allPoints[5] = new Points(null, 0);
-        allPoints[6] = new Points(Colour.B, 5);
-        allPoints[7] = new Points(null, 0);
-        allPoints[8] = new Points(null, 0);
-        allPoints[9] = new Points(null, 0);
-        allPoints[10] = new Points(null, 0);
-        allPoints[11] = new Points(Colour.W, 2);
-        allPoints[12] = new Points(Colour.B, 2);
-        allPoints[13] = new Points(null, 0);
-        allPoints[14] = new Points(null, 0);
-        allPoints[15] = new Points(null, 0);
-        allPoints[16] = new Points(null, 0);
-        allPoints[17] = new Points(Colour.W, 5);
-        allPoints[18] = new Points(null, 0);
-        allPoints[19] = new Points(Colour.W, 3);
-        allPoints[20] = new Points(null, 0);
-        allPoints[21] = new Points(null, 0);
-        allPoints[22] = new Points(null, 0);
-        allPoints[23] = new Points(Colour.B, 5);
+public static Dice[] setupDie() {
 
         Dice[] bothDie = new Dice[2];
         bothDie[0] = new Dice(0);
         bothDie[1] = new Dice(0);
 
-        boardSetup(allPoints, bothDie);
-
         return bothDie;
 
-    }
-
-private static void boardSetup(Points[] allPoints, Dice[] bothDie) {
-        Board.Draw(allPoints, bothDie);
     }
 
 public static Player setupPlayer(Scanner in) {
@@ -79,4 +41,36 @@ public static Player setupPlayer(Scanner in) {
     Player player= new Player(name,INITIAL_CHECKERS);
     return player;
     }
-}
+
+public static Points[] defaultBoard() {
+
+    Points[] allPoints = new Points[24];
+    allPoints[0] = new Points(Colour.W, 5);
+    allPoints[1] = new Points(null, 0);
+    allPoints[2] = new Points(null, 0);
+    allPoints[3] = new Points(null, 0);
+    allPoints[4] = new Points(Colour.B, 3);
+    allPoints[5] = new Points(null, 0);
+    allPoints[6] = new Points(Colour.B, 5);
+    allPoints[7] = new Points(null, 0);
+    allPoints[8] = new Points(null, 0);
+    allPoints[9] = new Points(null, 0);
+    allPoints[10] = new Points(null, 0);
+    allPoints[11] = new Points(Colour.W, 2);
+    allPoints[12] = new Points(Colour.B, 2);
+    allPoints[13] = new Points(null, 0);
+    allPoints[14] = new Points(null, 0);
+    allPoints[15] = new Points(null, 0);
+    allPoints[16] = new Points(null, 0);
+    allPoints[17] = new Points(Colour.W, 5);
+    allPoints[18] = new Points(null, 0);
+    allPoints[19] = new Points(Colour.W, 3);
+    allPoints[20] = new Points(null, 0);
+    allPoints[21] = new Points(null, 0);
+    allPoints[22] = new Points(null, 0);
+    allPoints[23] = new Points(Colour.B, 5);
+
+    return allPoints;
+
+    }
+}   
