57eb9e93650c26264392bf3ba234c3fdd4e37625
jeremytrane <jeremy.trane@ucdconnect.ie>
2022-11-03 11:25:39 +0000

    updated UserInput

--- Controller/UserInput.java
+++ Controller/UserInput.java
@@ -7,11 +7,5 @@ public class UserInput {
     public UserInput(String userInput) {
         this.userInput = userInput;
     }
-
-    public void RollDice(String userInput) {
-        if(userInput.equalsIgnoreCase("R")) {
-            System.out.println("Add code here to roll the dice!");
-        }
-    }
     
 }
