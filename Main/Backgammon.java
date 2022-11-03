2561cdc61ad28898c3236b0002278de8c40beabd
jeremytrane <jeremy.trane@ucdconnect.ie>
2022-11-03 16:10:52 +0000

    new main

--- Main/Backgammon.java
+++ Main/Backgammon.java
@@ -1,12 +1,51 @@
 package Main;
 
-import Controller.Game;
+import java.util.Scanner;
+
+import Model.Dice;
+import Model.Player;
+import Model.Points;
+import Setup.Setup;
+import View.Board;
 
 public class Backgammon {
 
     public static void main(String[] args) { 
 
-        Game.PlayGame();
+        Setup.setupMessage();
+
+        Scanner in = new Scanner(System.in);
+
+        Dice[] bothDie = Setup.setupDie();
+        Points[] allPoints = Setup.defaultBoard();
+        
+        System.out.println("Please enter name of player 1:");
+        Player player_1 = Setup.setupPlayer(in);
+        System.out.println("Please enter name of player 2:");
+        Player player_2 = Setup.setupPlayer(in);
+
+        Setup.getFirstTurn(bothDie,player_1,player_2);
+
+        while (player_1.getCheckerCount()!=0 || player_2.getCheckerCount()!=0) {
+
+            Board.Draw(allPoints, bothDie);
+
+            System.out.println("Please enter your move");
+            String move = in.nextLine();
+            if (move.equalsIgnoreCase("Q")) {
+                break;
+            } else if (move.equalsIgnoreCase("R")) {
+                Dice.rollDie(bothDie);
+            }
+        }
+
+        in.close();
 
     }
+    
+    public static void swapTurn(Player player1, Player player2){
+        player1.setTurn(-player1.getTurn());
+        player2.setTurn(-player2.getTurn());
+    }
+    
 }
