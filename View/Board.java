9d815444746c4b2d7fc1407f81af0ddf7db17a10
jeremytrane <jeremy.trane@ucdconnect.ie>
2022-11-03 16:39:47 +0000

    up

--- View/Board.java
+++ View/Board.java
@@ -8,10 +8,6 @@ import java.util.Arrays;
 
 public class Board {
 
-    public Board() {
-
-    }
-
     public static void Draw(Points[] allPoints, Dice[] bothDie, Player player_1, Player player_2) {
         int length1 = lengthToPrint1(allPoints);
         int length2 = lengthToPrint2(allPoints);
@@ -113,23 +109,6 @@ public class Board {
         return player_1;
         } else 
     return player_2;
-    }
-
-    public static void Draw1(Points[] allPoints, Dice[] bothDie){
-
-        System.out.println("||   1      2      3      4      5      6       7      8      9      10     11     12");
-        System.out.println("-----------------------------------------------------------------------------------------");
-        System.out.println("||\\  "+allPoints[0].getHowManyCheckers()+" / \\  "+allPoints[1].getHowManyCheckers()+" / \\  "+allPoints[2].getHowManyCheckers()+" / \\  "+allPoints[3].getHowManyCheckers()+" / \\  "+allPoints[4].getHowManyCheckers()+" / \\  "+allPoints[5].getHowManyCheckers()+" /|||\\  "+allPoints[6].getHowManyCheckers()+" / \\  "+allPoints[7].getHowManyCheckers()+" / \\  "+allPoints[8].getHowManyCheckers()+" / \\  "+allPoints[9].getHowManyCheckers()+" / \\  "+allPoints[10].getHowManyCheckers()+" / \\  "+allPoints[11].getHowManyCheckers()+" /||");
-        System.out.println("|| \\  /   \\  /   \\  /   \\  /   \\  /   \\  / ||| \\  /   \\  /   \\  /   \\  /   \\  /   \\  / ||");
-        System.out.println("||  \\/     \\/     \\/     \\/     \\/     \\/  |||  \\/     \\/     \\/     \\/     \\/     \\/  ||");
-        System.out.println("||                                         |||                                         ||   ["+bothDie[0].getDots() +"]");
-        System.out.println("||                                         |||                                         ||   ["+bothDie[1].getDots() +"]");
-        System.out.println("||  /\\     /\\     /\\     /\\     /\\     /\\  |||  /\\     /\\     /\\     /\\     /\\     /\\  ||");
-        System.out.println("|| /  \\   /  \\   /  \\   /  \\   /  \\   /  \\ ||| /  \\   /  \\   /  \\   /  \\   /  \\   /  \\ ||");
-        System.out.println("||/  "+allPoints[23].getHowManyCheckers()+" \\ /  "+allPoints[22].getHowManyCheckers()+" \\ /  "+allPoints[21].getHowManyCheckers()+" \\ /  "+allPoints[20].getHowManyCheckers()+" \\ /  "+allPoints[19].getHowManyCheckers()+" \\ /  "+allPoints[18].getHowManyCheckers()+" \\|||/  "+allPoints[17].getHowManyCheckers()+" \\ /  "+allPoints[16].getHowManyCheckers()+" \\ /  "+allPoints[15].getHowManyCheckers()+" \\ /  "+allPoints[14].getHowManyCheckers()+" \\ /  "+allPoints[13].getHowManyCheckers()+" \\ /  "+allPoints[12].getHowManyCheckers()+" \\||");
-        System.out.println("-----------------------------------------------------------------------------------------");
-        System.out.println("||  24     23     22     21     20     19       18     17     16     15     14     13");
-        
     }
 
 }
