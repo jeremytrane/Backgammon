20e435c3c3d96d833d19562f4af1739bc9f3bf3f
JamieMadden38 <jamiemadden38@gmail.com>
2022-12-02 18:06:07 +0000

    Added test for point creation and player creation

--- Tests/SetupTest.java
+++ Tests/SetupTest.java
@@ -2,6 +2,10 @@ package Tests;
 
 import Setup.Setup;
 import Model.Dice;
+import Model.Player;
+import Model.Points;
+
+import java.util.Scanner;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
@@ -10,9 +14,25 @@ import org.junit.Test;
 public class SetupTest {
     @Test
     // @DisplayName("Checks that dice are created.")
-    public void testDie() {
+    public void testDieCreation() {
         Dice[] bothDie = Setup.setupDie();
         assertNotNull(bothDie[0]);
         assertNotNull(bothDie[1]);
+    }
+
+    public void testPlayerCreation(){
+        Scanner in = new Scanner(System.in);
+        Player player = Setup.setupPlayer(in);
+        assertNotNull(player);
+    }
+
+    public void testPointCreation(){
+        Points[] points = Setup.defaultBoard();
+
+        for(Points p : points){
+            assertNotNull(p);
+        }
+
+        assertEquals(28, points.length);
     }
 }
