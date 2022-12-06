9d54bed591ad2d96a534173277a55640308ddee1
JamieMadden38 <115639800+JamieMadden38@users.noreply.github.com>
2022-12-06 17:16:32 +0000

    Added CheckersTest

--- Tests/CheckersTest.java
+++ Tests/CheckersTest.java
@@ -3,10 +3,19 @@ package Tests;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import Model.Checkers;
+import Model.Colour;
+
 public class CheckersTest {
+    Checkers checker = new Checkers(Colour.W);
+
     @Test
     // @DisplayName("Checks that ")
-    public void test() {
-        //assertNotNull();
+    public void testColourSetGet() {
+        Colour colourToSet = Colour.B;
+        Colour gotColour;
+        checker.setCheckerColour(colourToSet);
+        gotColour = checker.getCheckerColour();
+        assertEquals(colourToSet, gotColour);
     }
 }
